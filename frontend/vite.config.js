e1d67d69b8359153b801b5ac652a28ea69ca2a39
ghulambaseeruddin1 <ghulamnaseeruddin137@gmail.com>
2026-08-07 15:33:49 +0500

    build setting

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,17 +7,9 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        // Uses your Railway environment variable or falls back to localhost for your computer
         target: process.env.VITE_API_PROXY_TARGET || "http://localhost:4000",
         changeOrigin: true,
       },
     },
   },
-  preview: {
-    // This allows Railway to safely read the port and display your site online
-    host: true,
-    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
-  }
-});
-
 });
